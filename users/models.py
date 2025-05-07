4c9b7805afe3ceb7f89ca8d9c4eb190b42ec1e1e
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-05-08 00:21:18 +0300

    Add initial user app

---
+++ users/models.py
@@ -0,0 +1,32 @@
+"""Module to define CustomUser class"""
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from events.models import Event
+
+
+class CustomUser(AbstractUser):
+    """Custom user model extending the default Django user model
+    To represent users"""
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['username'])
+        ]
+
+
+class Booking(models.Model):
+    """Model to represent a booking"""
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name='bookings')
+
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, related_name='bookings')
+
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'event')
