a8fee340c133e6772276da10868199be75852c83
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-05-09 15:35:24 +0300

    Fix Typo

--- Config/settings.py
+++ Config/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -140,4 +140,3 @@ STATICFILES_DIRS = (
 )
 
 AUTH_USER_MODEL = 'users.CustomUser'
-
