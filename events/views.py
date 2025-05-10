e102cece07337f32e83ddb81a8974ee4b02c1423
eyadfattah23 <eyadabdelfattah4@gmail.com>
2025-05-10 15:48:10 +0300

    Add event URLs and views for home and about pages

--- events/views.py
+++ events/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .models import Event
 # Create your views here.
+
+
+def home(request):
+    query_set = Event.objects.all().select_related('creator')[:10]
+    return render(request, 'home.html', context={'name': 'eyad', 'events': query_set, 'title': 'Home'})
+
+
+def about(request):
+    return render(request, 'about.html', context={'title': 'About'})
